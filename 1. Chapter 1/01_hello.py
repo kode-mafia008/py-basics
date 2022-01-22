c197a91481986922cd508a12e845a7effb6a539f
Devs@HathTech <dev@hathtech.com>
2022-01-22 21:33:10 +0545

    chapter one completed

--- 1. Chapter 1/01_hello.py
+++ 1. Chapter 1/01_hello.py
@@ -3,5 +3,6 @@ Author: Harry
 Licenced to: ABC Company
 ***********Thanks for reading**********
 '''
-import os # importing the os module 
-print("Hello world")
+import os  # import os module
+print('Hello World')
+print('Initializing the practice from chapter one.')
